280c15987738bebc66bc2efb34215f916695aef2
classroom <classroom@K-LAPTOP-03.apiit.edu>
2019-09-09 11:17:24 +0530

    Implemented traverse

--- linked_list.py
+++ linked_list.py
@@ -15,9 +15,27 @@ class LinkedList:
         else:
             return True
 
-    def insert_front(self, node):
+    def insert_front(self, new_node):
         if self.is_empty():
-            self.head = node
-            self.tail = node
+            self.head = new_node
+            self.tail = new_node
         else:
-            
+            new_node.next_node = self.head
+            self.head = new_node
+
+    def traverse(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next_node
+
+
+list_1 = LinkedList()
+print(list_1.is_empty())
+
+list_1.insert_front(Node(data=5))
+list_1.insert_front(Node(data=3))
+print(list_1.is_empty())
+
+list_1.traverse()
+
